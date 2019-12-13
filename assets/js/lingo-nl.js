76eb7e20b2802f446d36278d38079ee06f9907f2
timo lemmen <99058056@mydavinci.nl>
2019-12-13 09:04:06 +0100

    lingo layout

---
+++ assets/js/lingo-nl.js
@@ -0,0 +1,626 @@
+var input = document.getElementById('vakje');
+document.getElementById('row1');
+var words = [
+	"appel",
+	"aldus",
+	"afwas",
+	"aftel",
+	"aarde",
+	"armen",
+	"actie",
+	"apart",
+	"adres",
+	"avond",
+	"aders",
+	"alarm",
+	"boten",
+	"balen",
+	"beter",
+	"bomen",
+	"boren",
+	"boven",
+	"boxen",
+	"brood",
+	"broek",
+	"brand",
+	"breed",
+	"benen",
+	"beeld",
+	"brief",
+	"beten",
+	"basis",
+	"blauw",
+	"beren",
+	"buren",
+	"banen",
+	"bloed",
+	"broer",
+	"blond",
+	"boter",
+	"beleg",
+	"breng",
+	"baken",
+	"beker",
+	"blind",
+	"bezig",
+	"baden",
+	"bedel",
+	"bazen",
+	"bazin",
+	"baren",
+	"beden",
+	"beken",
+	"bezem",
+	"baard",
+	"bidet",
+	"breuk",
+	"conus",
+	"cello",
+	"creme",
+	"cloud",
+	"cacao",
+	"cadet",
+	"cavia",
+	"ceder",
+	"combi",
+	"china",
+	"clown",
+	"draai",
+	"deden",
+	"dalen",
+	"derde",
+	"delen",
+	"dwaas",
+	"daden",
+	"dader",
+	"dames",
+	"diner",
+	"datum",
+	"dozen",
+	"dreun",
+	"duits",
+	"dagen",
+	"deren",
+	"dwerg",
+	"dwaal",
+	"dwing",
+	"druil",
+	"droog",
+	"draad",
+	"dweil",
+	"drank",
+	"duren",
+	"dwars",
+	"drugs",
+	"daten",
+	"daler",
+	"doorn",
+	"disco",
+	"degen",
+	"droom",
+	"dient",
+	"drone",
+	"dadel",
+	"duwen",
+	"druif",
+	"deken",
+	"deler",
+	"elven",
+	"eigen",
+	"enger",
+	"engel",
+	"elder",
+	"enkel",
+	"effen",
+	"email",
+	"egaal",
+	"fiets",
+	"friet",
+	"files",
+	"forel",
+	"films",
+	"feest",
+	"fruit",
+	"falen",
+	"flora",
+	"fauna",
+	"feeen",
+	"freak",
+	"forum",
+	"fusie",
+	"geven",
+	"gaven",
+	"groen",
+	"graai",
+	"getal",
+	"grens",
+	"grond",
+	"groef",
+	"graal",
+	"gewei",
+	"games",
+	"grote",
+	"groet",
+	"garen",
+	"gebak",
+	"graag",
+	"genre",
+	"glans",
+	"geluk",
+	"geeuw",
+	"horen",
+	"heren",
+	"halen",
+	"hagel",
+	"haren",
+	"helen",
+	"harde",
+	"hemel",
+	"hoofd",
+	"huren",
+	"hamer",
+	"haken",
+	"heden",
+	"hotel",
+	"hobby",
+	"heler",
+	"hoger",
+	"ieder",
+	"index",
+	"immer",
+	"icoon",
+	"inlog",
+	"inzet",
+	"innig",
+	"jovel",
+	"jaren",
+	"jicht",
+	"jabot",
+	"jacht",
+	"jaden",
+	"jagen",
+	"jager",
+	"japon",
+	"jarig",
+	"jawel",
+	"jeans",
+	"jemig",
+	"jeugd",
+	"joint",
+	"jonas",
+	"joule",
+	"koken",
+	"kreet",
+	"koker",
+	"kerst",
+	"kegel",
+	"koude",
+	"kader",
+	"krent",
+	"kamer",
+	"kaars",
+	"kaart",
+	"kraan",
+	"krant",
+	"keren",
+	"kruid",
+	"kerel",
+	"kubus",
+	"kraal",
+	"kleur",
+	"kroon",
+	"klein",
+	"korst",
+	"klopt",
+	"kabel",
+	"kunst",
+	"kopje",
+	"krans",
+	"klimt",
+	"kater",
+	"klink",
+	"kudde",
+	"kruis",
+	"lopen",
+	"laten",
+	"lepel",
+	"links",
+	"laden",
+	"leven",
+	"lezen",
+	"lucht",
+	"lenen",
+	"laser",
+	"lente",
+	"licht",
+	"lader",
+	"leder",
+	"lunch",
+	"lijst",
+	"leger",
+	"leden",
+	"legen",
+	"lagen",
+	"lezer",
+	"lever",
+	"lingo",
+	"loper",
+	"luier",
+	"lager",
+	"leeuw",
+	"maand",
+	"malen",
+	"maken",
+	"media",
+	"meter",
+	"motor",
+	"maten",
+	"markt",
+	"mazen",
+	"molen",
+	"meest",
+	"meren",
+	"model",
+	"meden",
+	"maden",
+	"macht",
+	"meeuw",
+	"mager",
+	"magen",
+	"maren",
+	"manen",
+	"noord",
+	"nieuw",
+	"negen",
+	"namen",
+	"neven",
+	"nodig",
+	"naden",
+	"neder",
+	"nemen",
+	"onder",
+	"optel",
+	"ovaal",
+	"ovale",
+	"onwel",
+	"optie",
+	"orden",
+	"oppas",
+	"ouder",
+	"ophef",
+	"oases",
+	"palen",
+	"plein",
+	"pegel",
+	"paars",
+	"prijs",
+	"piano",
+	"pixel",
+	"paden",
+	"pasta",
+	"pizza",
+	"poten",
+	"paard",
+	"puber",
+	"pauze",
+	"preek",
+	"polis",
+	"pater",
+	"proef",
+	"panda",
+	"penis",
+	"prins",
+	"pluto",
+	"polen",
+	"plint",
+	"quota",
+	"quant",
+	"quark",
+	"queue",
+	"quilt",
+	"quote",
+	"robot",
+	"reken",
+	"raden",
+	"regen",
+	"radio",
+	"rente",
+	"regio",
+	"rugby",
+	"reden",
+	"roken",
+	"ruzie",
+	"ruist",
+	"regel",
+	"racen",
+	"races",
+	"riool",
+	"ramen",
+	"radar",
+	"roman",
+	"rokje",
+	"razen",
+	"roede",
+	"staan",
+	"staal",
+	"speel",
+	"steeg",
+	"stoel",
+	"stook",
+	"steek",
+	"schep",
+	"spijs",
+	"stoep",
+	"shirt",
+	"samen",
+	"sites",
+	"sport",
+	"spalk",
+	"sjaal",
+	"storm",
+	"staat",
+	"steun",
+	"strak",
+	"serie",
+	"shows",
+	"schat",
+	"snoep",
+	"sfeer",
+	"smeer",
+	"speer",
+	"scene",
+	"speld",
+	"smeed",
+	"smaak",
+	"super",
+	"stand",
+	"steer",
+	"smelt",
+	"sedan",
+	"skier",
+	"sluis",
+	"sneer",
+	"steel",
+	"truck",
+	"terug",
+	"typen",
+	"talen",
+	"taboe",
+	"tegel",
+	"taart",
+	"tafel",
+	"trouw",
+	"teken",
+	"teren",
+	"taken",
+	"treur",
+	"tenen",
+	"titel",
+	"thuis",
+	"tiara",
+	"teder",
+	"toets",
+	"tabak",
+	"trein",
+	"tarwe",
+	"telen",
+	"teler",
+	"uiten",
+	"uilig",
+	"uitje",
+	"uiver",
+	"ultra",
+	"uniek",
+	"uppie",
+	"uraan",
+	"uiers",
+	"velen",
+	"vloer",
+	"video",
+	"varen",
+	"vegen",
+	"veren",
+	"vader",
+	"vaten",
+	"vuren",
+	"vrouw",
+	"vlees",
+	"vogel",
+	"vroeg",
+	"vezel",
+	"veins",
+	"vorst",
+	"veder",
+	"vanaf",
+	"vieze",
+	"veger",
+	"villa",
+	"veler",
+	"vrede",
+	"vries",
+	"woord",
+	"wagen",
+	"wonen",
+	"waren",
+	"warme",
+	"weten",
+	"water",
+	"weren",
+	"wazig",
+	"wegen",
+	"weven",
+	"wezen",
+	"weken",
+	"wraak",
+	"wilde",
+	"wreed",
+	"wrede",
+	"wenst",
+	"woest",
+	"xenon",
+	"yacht",
+	"yucca",
+	"zwaar",
+	"zware",
+	"zesde",
+	"zagen",
+	"zalig",
+	"zomer",
+	"zeden",
+	"zwart",
+	"zeven",
+	"zicht",
+	"zadel",
+	"zweet",
+	"zenuw",
+	"zweer",
+	"zweef",
+	"zaden",
+	"zaken",
+	"zeker",
+	"zever",
+	"zeeen"];
+
+var randomWord = words[Math.floor(Math.random()*words.length)];
+
+var getAllIndexes = function(words, val) {
+    var indexes = [], i;
+    for(i = 0; i < words.length; i++)
+        if (words[i] === val)
+            indexes.push(i);
+  }
+
+document.getElementsByName('row1').doucment.getElementsByName=randomWord[0];
+
+
+
+function startGame(){
+
+}
+
+startGame();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var Snowflake = (function() {
+
+    var flakes;
+    var flakesTotal = 100;
+    var wind = 0;
+    var mouseX;
+    var mouseY;
+
+    function Snowflake(size, x, y, vx, vy) {
+        this.size = size;
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.hit = false;
+        this.melt = false;
+        this.div = document.createElement('div');
+        this.div.classList.add('snowflake');
+        this.div.style.width = this.size + 'px';
+        this.div.style.height = this.size + 'px';
+    }
+
+    Snowflake.prototype.move = function() {
+        if (this.hit) {
+            if (Math.random() > 0.995) this.melt = true;
+        } else {
+            this.x += this.vx + Math.min(Math.max(wind, -10), 10);
+            this.y += this.vy;
+        }
+
+        // Wrap the snowflake to within the bounds of the page
+        if (this.x > window.innerWidth + this.size) {
+            this.x -= window.innerWidth + this.size;
+        }
+
+        if (this.x < -this.size) {
+            this.x += window.innerWidth + this.size;
+        }
+
+        if (this.y > window.innerHeight + this.size) {
+            this.x = Math.random() * window.innerWidth;
+            this.y -= window.innerHeight + this.size * 2;
+            this.melt = false;
+        }
+
+        var dx = mouseX - this.x;
+        var dy = mouseY - this.y;
+        this.hit = !this.melt && this.y < mouseY && dx * dx + dy * dy < 2400;
+    };
+
+    Snowflake.prototype.draw = function() {
+        this.div.style.transform =
+        this.div.style.MozTransform =
+        this.div.style.webkitTransform =
+            'translate3d(' + this.x + 'px' + ',' + this.y + 'px,0)';
+    };
+
+    function update() {
+        for (var i = flakes.length; i--; ) {
+            var flake = flakes[i];
+            flake.move();
+            flake.draw();
+        }
+        requestAnimationFrame(update);
+    }
+
+    Snowflake.init = function(container) {
+        flakes = [];
+
+        for (var i = flakesTotal; i--; ) {
+            var size = (Math.random() + 0.2) * 12 + 1;
+            var flake = new Snowflake(
+                size,
+                Math.random() * window.innerWidth,
+                Math.random() * window.innerHeight,
+                Math.random() - 0.5,
+                size * 0.3
+            );
+            container.appendChild(flake.div);
+            flakes.push(flake);
+        }
+
+      container.ontouchstart = function(event) {
+          mouseX = event.targetTouches[0].clientX;
+          mouseY = event.targetTouches[0].clientY;
+          event.preventDefault();
+    };
+
+    window.ondeviceorientation = function(event) {
+        if (event) {
+            wind = event.gamma / 10;
+        }
+    };
+    
+    update();
+    };
+
+    return Snowflake;
+
+}());
+
+window.onload = function() {
+  setTimeout(function() {
+    Snowflake.init(document.getElementById('snow'));
+  }, 500);
+}
