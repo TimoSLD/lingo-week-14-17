f9c005591bbd3e44d75560a5c4b39371770d63cf
timo lemmen <99058056@mydavinci.nl>
2020-01-14 15:26:30 +0100

    nog steeds button

--- assets/js/lingo-nl.js
+++ assets/js/lingo-nl.js
@@ -2,41 +2,39 @@ var vakjes;
 
 const row1 = document.getElementById("row1");
 
-for(vakjes = 0; vakjes < 24; vakjes++){
+for(vakjes = 0; vakjes < 25; vakjes++){
     var newDiv = document.createElement("input");
-    newDiv.type = "text";
-    newDiv.readOnly;
     row1.appendChild(newDiv);
-    newDiv.style.background = "red";
-    newDiv.style.verticalAlign = 'top';
-    newDiv.style.marginTop = '4px';
+    newDiv.style.background = "blue";
+    newDiv.style.verticalAlign = top;
+    newDiv.style.marginTop = 4;
     newDiv.style.display = "inline-block";
     newDiv.style.height = '1.25em';
     newDiv.style.width = '1.25em';
     newDiv.style.textAlign = 'center';
     newDiv.style.fontSize = '3.5em';
-    newDiv.style.fontFamily = 'Lato', 'sans-serif'
+    newDiv.style.fontFamily = 'Lato', 'sans-serif';
+    newDiv.id += 'vakje'+ vakjes;
 }  
+
 
 var randomWord = words[Math.floor(Math.random() * words.length)];
 
 var textArray = randomWord.split('');
-document.getElementById('vakje1').value = textArray[0];
+document.getElementById('vakje0').value = textArray[0];
+console.log(textArray);
 
 const guess = document.getElementById('guess');
     let addGuess = document.createElement('input');
         addGuess.placeholder = 'typ hier.';
-        addGuess.style.display = 'inline-block';
         guess.appendChild(addGuess);
+
 
 const check = document.getElementById('buttoncheck');
     let addCheck = document.createElement('button');
         addCheck.textContent = 'check';
-        addCheck.style.display = 'inline-block';
         check.appendChild(addCheck);
-
-
-
+    
 
 
 
