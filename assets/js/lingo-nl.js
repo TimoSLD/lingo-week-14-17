a63fd927e57b195872537164329c1b05b0422104
timo lemmen <99058056@mydavinci.nl>
2020-01-15 14:37:55 +0100

    bijna klaar

--- assets/js/lingo-nl.js
+++ assets/js/lingo-nl.js
@@ -1,11 +1,14 @@
 var vakjes;
 
-const row1 = document.getElementById("row1");
 
-for(vakjes = 0; vakjes < 25; vakjes++){
+const row1 = document.getElementById("row1");
+var poging = 0
+
+for(rij = 0; rij < 5; rij++){
+    for(kolom = 0; kolom < 5; kolom++){
     var newDiv = document.createElement("input");
     row1.appendChild(newDiv);
-    newDiv.style.background = "blue";
+    newDiv.style.background = "white";
     newDiv.style.verticalAlign = top;
     newDiv.style.marginTop = 4;
     newDiv.style.display = "inline-block";
@@ -14,26 +17,43 @@ for(vakjes = 0; vakjes < 25; vakjes++){
     newDiv.style.textAlign = 'center';
     newDiv.style.fontSize = '3.5em';
     newDiv.style.fontFamily = 'Lato', 'sans-serif';
-    newDiv.id += 'vakje'+ vakjes;
+    newDiv.id += 'vakje'+ rij + '-' + kolom;
+    }
 }  
 
 
 var randomWord = words[Math.floor(Math.random() * words.length)];
 
 var textArray = randomWord.split('');
-document.getElementById('vakje0').value = textArray[0];
+document.getElementById('vakje0-0').value = textArray[0];
 console.log(textArray);
 
-const guess = document.getElementById('guess');
-    let addGuess = document.createElement('input');
-        addGuess.placeholder = 'typ hier.';
-        guess.appendChild(addGuess);
 
 
-const check = document.getElementById('buttoncheck');
-    let addCheck = document.createElement('button');
-        addCheck.textContent = 'check';
-        check.appendChild(addCheck);
+function check(){   
+  var antwoord = document.getElementById('antwoord');
+  var antwoord = antwoord.value.split('');
+   
+    if (antwoord.length == 5) {
+        for(i = 0; i < antwoord.length; i++){
+            if(antwoord[i] == textArray[i]) {
+                document.getElementById("vakje" + poging + "-" + i).value = antwoord[i];
+                document.getElementById("vakje" + poging + "-" + i).style.backgroundColor = "green";
+                textArray[i] = null;
+            } else if (textArray.indexOf(antwoord[i])>-1) {
+                document.getElementById("vakje" + poging + "-" + i).value = antwoord[i];
+                document.getElementById("vakje" + poging + "-" + i).style.backgroundColor = "yellow";
+            } else {
+                document.getElementById("vakje" + poging + "-" + i).value = antwoord[i];
+                document.getElementById("vakje" + poging + "-"+ i).style.backgroundColor = "red";
+            }
+        }
+    }
+    
+
+    poging++
+}
+
     
 
 
